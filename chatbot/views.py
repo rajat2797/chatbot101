13c72c6426931ba5cdc1d55e8c24f6429109b0ce
rajat2797 <rajatsinghal1997@gmail.com>
2016-09-07 17:27:33 +0530

    updates

--- chatbot/views.py
+++ chatbot/views.py
@@ -1,9 +1,13 @@
+# -*- coding: utf-8
+
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import generic
 from django.views.decorators.csrf import csrf_exempt
 from django.utils.decorators import method_decorator
 # Create your views here.
+
+VERIFY_TOKEN='7thseptember2016'
 
 class MyChatBotView(generic.View):
 	def get(self,request,*args,**kwargs):
