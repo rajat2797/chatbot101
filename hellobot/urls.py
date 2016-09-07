790a77b59658efb6ebd8c822fe7ad4732814822d
rajat2797 <rajatsinghal1997@gmail.com>
2016-09-07 17:56:07 +0530

    updates

--- hellobot/urls.py
+++ hellobot/urls.py
@@ -8,5 +8,5 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', index),
-    url(r'^facebook_auth/', MyChatBotView.as_view()),
+    url(r'^facebook_auth$', MyChatBotView.as_view()),
 )
