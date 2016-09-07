6346b083b84d6dae1c79750f56088da454007318
rajat2797 <rajatsinghal1997@gmail.com>
2016-09-07 19:01:01 +0530

    updates

--- chatbot/views.py
+++ chatbot/views.py
@@ -6,6 +6,7 @@ from django.views import generic
 from django.views.decorators.csrf import csrf_exempt
 from django.utils.decorators import method_decorator
 import json
+import requests
 # Create your views here.
 
 VERIFY_TOKEN='7thseptember2016'
