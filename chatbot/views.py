f9b17c408354fc126d7d05f9efaa4e652556ed40
rajat2797 <rajatsinghal1997@gmail.com>
2016-09-07 18:54:32 +0530

    updates

--- chatbot/views.py
+++ chatbot/views.py
@@ -10,6 +10,14 @@ import json
 
 VERIFY_TOKEN='7thseptember2016'
 
+PAGE_ACCESS_TOKEN='EAAJmjf94eZB8BAEJHwLBtA5RxiIR6WUhra7TiXXIZBHrFtV7ZCyUFGuPOpG2O9vWMa2Lc8w5IFQZA1aZCHPqP4eZCrZCAcGQgYrcubYnVcD2jGF8ems2ZAUfQARhR6ivnofruOF2cSLKVVGEW8lOcYYh2FZBZCioJFDeHnZAy5PKcu1oQZDZD'
+
+def post_facebook_message(fbid,message_text):
+	post_message_url = 'https://graph.facebook.com/v2.6/me/messages?access_token=%s'%PAGE_ACCESS_TOKEN
+	response_msg = json.dumps({"recipient":{"id":fbid}, "message":{"text":message_text}})
+	status = requests.post(post_message_url, headers={"Content-Type": "application/json"},data=response_msg)
+	print status.json()
+
 class MyChatBotView(generic.View):
 	def get(self,request,*args,**kwargs):
 		if self.request.GET['hub.verify_token']==VERIFY_TOKEN:
@@ -24,6 +32,12 @@ class MyChatBotView(generic.View):
 	def post(self,request,*args,**kwargs):
 		incoming_message=json.loads(self.request.body.decode('utf-8'))
 		print incoming_message
+
+		for entry in incoming_message['entry']:
+			for message in entry['messaging']:
+				print message
+				message_text = message['message']['text']
+				post_facebook_message(sender_id,message_text) 
 
 def index(request):
 	return HttpResponse('hello')
