fede9cff8051b912e3e7d2e5cdddad1fa10ab4a4
rajat2797 <rajatsinghal1997@gmail.com>
2016-09-10 09:34:18 +0530

    foo

--- chatbot/views.py
+++ chatbot/views.py
@@ -94,7 +94,7 @@ class MyChatBotView(generic.View):
 						post_facebook_message(sender_id,message_text[1])
 					elif '#wiki' in message_text.lower():
 						message_text = message_text.split(" ",1)
-						wikisearch(sender_id,message_text[1])
+						wikisearch(sender_id,message_text[1].replace(' ',''))
 				except Exception as e:
 					print e
 					pass
