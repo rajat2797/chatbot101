c50dd2eede221cffb1fe7458d1995f4d8f859d29
rajat2797 <rajatsinghal1997@gmail.com>
2016-09-11 13:13:44 +0530

    blog

--- chatbot/models.py
+++ chatbot/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 
-class users(models.Model):
+class Users(models.Model):
 	user_name = models.CharField(max_length=128, unique=True)
 	password = models.CharField(max_length=128)
 	title = models.CharField(max_length=128)
